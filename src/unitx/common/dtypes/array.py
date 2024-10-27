20a31e3bcba12ed18bcce980e5bc139ec46557b0
snorflib <modicum.geotag.0v@icloud.com>
2024-10-27 17:11:47 +0100

    feat: added array params validators

--- src/unitx/common/dtypes/array.py
+++ src/unitx/common/dtypes/array.py
@@ -1,9 +1,15 @@
 import attrs
+from attrs import validators
 
 from . import base
 
 
 @attrs.frozen(kw_only=True)
 class Array(base.AbstractDType):
-    size: int
-    granularity: int = 1
+    size: int = attrs.field(
+        validator=validators.gt(0),
+    )
+    granularity: int = attrs.field(
+        validator=validators.gt(0),
+        default=1,
+    )
