ec0630ec10fc047dfaf09eaae2bad1a8a96ddfe0
snorflib <modicum.geotag.0v@icloud.com>
2024-10-27 17:11:59 +0100

    chore: dtype tests

---
+++ tests/unitx/common/dtypes.py
@@ -0,0 +1,35 @@
+import typing
+
+import pytest
+
+from src.unitx.common import dtypes
+
+
+def test_same_constant_state() -> None:
+    const1 = dtypes.Constant(value=1)
+    const2 = dtypes.Constant(value=1)
+    assert const1 is const2
+
+
+def test_diff_constant_state() -> None:
+    const1 = dtypes.Constant(value=1)
+    const2 = dtypes.Constant(value=2)
+    assert const1 is not const2
+
+
+def test_unit_hashability() -> None:
+    unit = dtypes.Unit()
+    assert isinstance(unit, typing.Hashable)
+
+
+def test_array_creation_correct() -> None:
+    arr = dtypes.Array(size=10, granularity=1)
+    assert isinstance(arr, typing.Hashable)
+
+
+def test_array_incorrect_creation() -> None:
+    with pytest.raises(Exception) as _:
+        dtypes.Array(size=-1)
+
+    with pytest.raises(Exception) as _:
+        dtypes.Array(size=10, granularity=-1)
