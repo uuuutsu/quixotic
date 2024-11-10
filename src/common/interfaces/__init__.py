2cf257f1daa28894187dd041b1ac64a690e4861b
snorflib <modicum.geotag.0v@icloud.com>
2024-11-10 19:11:44 +0100

    feat: deleted OpCode type from common

--- src/common/interfaces/__init__.py
+++ src/common/interfaces/__init__.py
@@ -3,11 +3,9 @@ __all__ = (
     "PrintableType",
     "BuilderType",
     "ChainType",
-    "OpcodeType",
 )
 
 from .builder import BuilderType
 from .chain import ChainType
-from .opcode import OpcodeType
 from .printable import PrintableType
 from .visitor import VisitorType
