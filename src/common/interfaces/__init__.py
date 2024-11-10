83535fcf4a462078cb4436b79f22562491a8862d
snorflib <modicum.geotag.0v@icloud.com>
2024-11-10 02:42:37 +0100

    feat: chain

--- src/common/interfaces/__init__.py
+++ src/common/interfaces/__init__.py
@@ -2,10 +2,12 @@ __all__ = (
     "VisitorType",
     "PrintableType",
     "BuilderType",
+    "ChainType",
     "OpcodeType",
 )
 
 from .builder import BuilderType
+from .chain import ChainType
 from .opcode import OpcodeType
 from .printable import PrintableType
 from .visitor import VisitorType
