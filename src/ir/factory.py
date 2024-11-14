dfc2fb61d25ecc4fbc4c267c9b9b7a3075305d09
snorflib <modicum.geotag.0v@icloud.com>
2024-11-14 22:58:40 +0100

    feat: ir into a first level module

--- src/ir/factory.py
+++ src/ir/factory.py
@@ -7,20 +7,20 @@ import attrs
 
 from src.common import tools
 
-from . import base
+from . import opcode
 
 P = typing.ParamSpec("P")
 R = typing.TypeVar("R")
 
 
 @tools.BaseChain
-def signature_to_opcode(func: typing.Callable[P, None]) -> typing.Callable[P, dict[str, typing.Any]]:
+def signature_to_opcode(func: typing.Callable[P, None]) -> typing.Callable[P, opcode.BaseOpcode]:
     annot = typing.get_type_hints(func)
     annot.pop("return", None)
 
     cls = type(
         func.__name__,
-        (base.BaseOpcode,),
+        (opcode.BaseOpcode,),
         {
             "__annotations__": annot,
             "__attrs__": func.__code__.co_varnames,
@@ -30,7 +30,7 @@ def signature_to_opcode(func: typing.Callable[P, None]) -> typing.Callable[P, di
     echo = tools.signature_to_echo(func)
 
     @functools.wraps(func)
-    def _inner(*args: P.args, **kwargs: P.kwargs) -> base.BaseOpcode:
-        return typing.cast(base.BaseOpcode, opcode_cls(**echo(*args, **kwargs)))
+    def _inner(*args: P.args, **kwargs: P.kwargs) -> opcode.BaseOpcode:
+        return typing.cast(opcode.BaseOpcode, opcode_cls(**echo(*args, **kwargs)))
 
     return _inner
