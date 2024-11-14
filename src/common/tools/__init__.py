bd8257930f1f5848d73f10ba852daa191ae7563a
snorflib <modicum.geotag.0v@icloud.com>
2024-11-14 22:54:57 +0100

    feat: sig2echo function

--- src/common/tools/__init__.py
+++ src/common/tools/__init__.py
@@ -7,6 +7,7 @@ __all__ = (
     "camel_case_to_snake_case",
     "BaseChain",
     "dp_factory",
+    "signature_to_echo",
 )
 
 from .camel2snake import camel_case_to_snake_case
@@ -15,5 +16,6 @@ from .dispatcher import dp_factory
 from .flyweight import FlyweightMeta
 from .get_root import get_root_folder
 from .same import same_as
+from .sig2echo import signature_to_echo
 from .singleton import SingletonMeta
 from .unique import generate_unique_id
---
+++ src/common/tools/sig2echo.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+import functools
+import inspect
+import typing
+
+P = typing.ParamSpec("P")
+
+
+def signature_to_echo(func: typing.Callable[P, None]) -> typing.Callable[P, dict[str, typing.Any]]:
+    @functools.wraps(func)
+    def _echo(*args: P.args, **kwargs: P.kwargs) -> dict[str, typing.Any]:
+        bounded_signature = inspect.signature(func).bind(*args, **kwargs)
+        bounded_signature.apply_defaults()
+        return typing.cast(dict[str, typing.Any], bounded_signature.arguments)
+
+    return _echo
